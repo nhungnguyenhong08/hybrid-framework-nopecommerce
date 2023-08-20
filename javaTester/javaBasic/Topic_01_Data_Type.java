b18ecfd9babbef1b5372a0229951eaa0d9d46c77
Nhung Nguyen <nhungnth8.neu@gmail.com>
2023-08-20 16:46:33 +0700

    commit demo java basic Topic 01 Data type

--- javaTester/javaBasic/Topic_01_Data_Type.java
+++ javaTester/javaBasic/Topic_01_Data_Type.java
@@ -1,5 +1,60 @@
 package javaBasic;
 
-public class Topic_01_Data_Type {
+import java.util.Scanner;
 
+public class Topic_01_Data_Type {
+	
+	// Hàm khởi tạo
+	public Topic_01_Data_Type(){
+	}
+	
+	// Biến global
+	// K cần khởi tạo object mà chỉ cần dùng thông qua gọi ten_class.ten_bien
+	static int studentNumber;
+	static boolean statusValue;
+	
+	// static + final => hằng số(constant): phải viết hoa và phân cách bởi dấu _
+	static final String BROWSER_NAME = "Chrome";
+	
+	static int studentPrice;
+	
+	String studentName ="Automation FC";
+	
+	public static void main (String []agrs) {
+		
+		  System.out.println(studentNumber); System.out.println(statusValue);
+		  System.out.println(Topic_01_Data_Type.BROWSER_NAME);
+		  System.out.println(Topic_01_Data_Type.statusValue);
+		  System.out.println(Topic_01_Data_Type.studentNumber);
+		 
+		
+		int studentPrice =5;
+		System.out.println(studentPrice);
+		
+		// Khai báo 1 object 
+		// Để dùng hàm global thông qua 1 object đã được khởi tạo
+		Topic_01_Data_Type topic = new Topic_01_Data_Type();
+		System.out.println(topic.studentName);
+		
+		System.out.println(Topic_01_Data_Type.BROWSER_NAME);
+		
+		
+		// input:khi nào run thì mới nhập dữ liệu vào thông qua console
+		Scanner scanner = new Scanner (System.in);
+		String name = scanner.nextLine();
+		System.out.println(name);
+		System.out.println(name);
+		System.out.print(name);
+		System.out.print(name);
+	}
+	
+	// Getter: getCurrentUrl/getTittle/getAttribute/getText/getSize/getCSSValue/getLocation/getPossition/...
+	public String getStudentName(){
+		return this.studentName;
+	}
+	
+	// Setter: click/sendkey/clear/select/back/forward/refresh/get/....(liên quan tới các action)
+	public void setStudentName (String stdName) {
+		this.studentName = stdName;
+	}
 }
--- testcase/com/nopcommerce/user/User_01_Register_Login.java
+++ testcase/com/nopcommerce/user/User_01_Register_Login.java
@@ -6,6 +6,9 @@ import org.testng.annotations.AfterClass;
 
 
 public class User_01_Register_Login {
+	// Biến toàn cục: phạm vi dùng trong cả class test (màu xanh)
+	String homePageUrl ="";
+	
 	@BeforeClass
 	public void beforeClass() {
 	  
@@ -13,11 +16,23 @@ public class User_01_Register_Login {
 
   @Test
   public void TC_01() {
+	  // Biến cục bộ: chỉ sử dụng trong phạm vi testcase 01 (Hàm) này (màu đỏ)
+	  String homePageUrl = "";
+	  System.out.println(homePageUrl);
+	  
+	  // Block code
+	  if (3<5) {
+		  // Biến cục bộ: chỉ dùng trong hàm IF
+		  String homePageTitle ="";
+		  System.out.println(homePageTitle);
+		
+	}
   }
   
   
   @Test
   public void TC_02() {
+	  System.out.println(homePageUrl);
   }
   
   
